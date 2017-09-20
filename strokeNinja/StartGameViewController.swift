9a615e66b0d0f27ae12ce0c148f69e775fda0a8c
Andrew Lau <andrew@Andrews-MacBook-Pro.local>
2017-09-19 19:56:44 -0700

    Added Facebook login

---
+++ strokeNinja/StartGameViewController.swift
@@ -0,0 +1,23 @@
+//
+//  StartGameViewController.swift
+//  strokeNinja
+//
+//  Created by Andrew Lau on 9/19/17.
+//  Copyright © 2017 Toro Roan. All rights reserved.
+//
+
+import UIKit
+
+class StartGameViewController: UIViewController {
+
+    var id: String?
+    var name: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(name)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- strokeNinja/ViewController.swift
+++ strokeNinja/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
+import FacebookLogin
+import FBSDKLoginKit
+
+
+
 class ViewController: UIViewController {
 
+    var dict : [String : AnyObject]!
+    
+    @IBOutlet weak var login: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                self.getFBUserData()
+            }
+        }
+    }
+    
+    //function is fetching the user data
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    //print(result!)
+                    //print(self.dict)
+                    
+                    //create user if user does not exist
+                    
+                    self.performSegue(withIdentifier: "login", sender: nil)
+                }
+            })
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "login"{
+            let startGame = segue.destination as! StartGameViewController
+            startGame.name = dict["name"] as? String
+        }
     }
-
-
 }
 
